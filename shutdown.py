e981837f43dbbdf3eccb63bab5ffdf17a8f15c19
dan.dep <dan.depietri@gmail.com>
2023-09-23 23:23:55 +0200

    debugging shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -1,40 +1,45 @@
 #!/usr/bin/env python
 import os
-import sys
 import paho.mqtt.client as mqtt
 import time
-broker="octopi.local"
-port=1883
 
-#define callback
+broker = "octopi.local"
+port = 1883
+
 def on_message(client, userdata, message):
-   msg=str(message.payload.decode("utf-8"))
-   print("message =",msg)
-   topic=message.topic
-   messages.append([topic,msg])
-def on_connect(client, userdata, flags,rc):
-    if rc==0:
-        client.connected_flag=True
+    msg = str(message.payload.decode("utf-8"))
+    print("message =", msg)
+    topic = message.topic
+    print("Received message on topic:", topic)
+    # Execute shutdown command here
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        client.connected_flag = True
         client.subscribe(sub_topic)
     else:
-        client.bad_connection_flag=True
-        client.connected_flag=False
+        client.bad_connection_flag = True
+        client.connected_flag = False
 
-## MQTT
-messages=[]
-sub_topic="octoprint/shutdownscript"
-client= mqtt.Client("Octoprint")
-######
-client.on_message=on_message
-client.on_connect=on_connect
-## client.username_pw_set(username="admin", password="admin")
-client.connected_flag=False
-client.connect(broker, port=port)#connect
-while True:
-   client.loop(0.01)
-   time.sleep(1)
-   if len(messages)>0:
-      m=messages.pop(0)
-      print("shutdown")
-      ## client.publish("sonoff/mains", payload=None, qos=0, retain=False)
-      os.system("sudo shutdown now")
+# MQTT
+sub_topic = "octoprint/shutdownscript"
+client = mqtt.Client("Octoprint")
+client.on_message = on_message
+client.on_connect = on_connect
+# client.username_pw_set(username="admin", password="admin")
+
+client.connected_flag = False
+try:
+    client.connect(broker, port=port)  # Connect to the broker
+    client.loop_start()  # Start the MQTT loop
+
+    while client.connected_flag:
+        time.sleep(1)  # Your script will continue running here
+except KeyboardInterrupt:
+    print("Keyboard interrupt detected. Exiting.")
+    client.disconnect()
+    client.loop_stop()
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
+
+print("Script finished.")
